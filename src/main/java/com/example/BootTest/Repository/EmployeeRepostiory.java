dd9a85a4a8a0e870dad2df658106f5937c01ef26
Mohit <mohitkumarchourasia2002@gmail.com>
2026-01-05 17:53:17 +0530

    Revert "Initial Commit : A Rough project to do practise"
    This reverts commit c533528eaba90270ee1a5b0d69c78b28d42942ae.

--- src/main/java/com/example/BootTest/Repository/EmployeeRepostiory.java
+++
@@ -1,9 +0,0 @@
-package com.example.BootTest.Repository;
-
-import com.example.BootTest.Entity.EmployeeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepostiory extends JpaRepository<EmployeeEntity,Long> {
-}
